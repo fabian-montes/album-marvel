094c37f03eda28e8789e668607af8d6586d48f1b
fabianmontes <fabianmontes11fab@gmail.com>
2024-07-21 00:12:45 -0600

    primer commit

---
+++ src/main/java/com/marvel/albumdigital/AlbumdigitalApplication.java
@@ -0,0 +1,13 @@
+package com.marvel.albumdigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumdigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumdigitalApplication.class, args);
+	}
+
+}
